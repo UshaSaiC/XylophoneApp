7404947b8100ac0e5640e888ad26dfe1dbec3eb0
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-04 14:29:47 +0530

    Every key tapped leads to  different tune

--- XylophoneApp/ViewController.swift
+++ XylophoneApp/ViewController.swift
@@ -12,14 +12,11 @@ class ViewController: UIViewController {
     
     var player: AVAudioPlayer!
 
-    func playSound() {
-        // getting the location of C.wav file
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         
-        // keeping the file (url) into player
         player = try! AVAudioPlayer(contentsOf: url!)
         
-        // playing the sound
         player.play()
     }
 
@@ -28,21 +25,8 @@ class ViewController: UIViewController {
     }
 
 
-    //IBAction (Interface Builder Action) is a funtion which is not called by us but when an action happens on UI linked to the IBAction.. then the block of code gets executed
-    // (_ sender: UIButton) --> inputs to function
-    // Dragging the outlet connection formed in this file to all other keys on UI of Main.storyboard
-    //Now linked all the 7 buttons to below IBAction
     @IBAction func cKeyClicked(_ sender: UIButton) {
-        print(sender.backgroundColor)// prints the color of sender button hich is basically the button tapped by user
-        
-        /*
-         We can get the title of button in 3 ways :
-         1. sender.title(for: .normal)
-         2. sender.titleLabel?.text
-         3. sender.currentTitle
-        */
-        print(sender.currentTitle)
-        playSound()
+        playSound(soundName: sender.currentTitle!)
     }
 }
 
