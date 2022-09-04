13f6ed3672a6237d04780e46d12bf9ab740faf38
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-04 13:36:23 +0530

    Simplified playsound function and made use of same IBAction for all button being tapped

--- XylophoneApp/ViewController.swift
+++ XylophoneApp/ViewController.swift
@@ -10,39 +10,38 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
-    var player: AVAudioPlayer?
+    var player: AVAudioPlayer!
 
-    // below function is copy pasted from stack over flow
     func playSound() {
+        // getting the location of C.wav file
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
         
-        // getting C.wav file from the resources in this project using Bundle.main keyword
-        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
-
-        do {
-            // making sure that audio is played even when phone is silent via using category as .playback
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-
-            // telling audio player that we have to play url file i.e C.wav file
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-
-            guard let player = player else { return }
-
-            player.play()
-
-        } catch let error {
-            print(error.localizedDescription)
-        }
+        // keeping the file (url) into player
+        player = try! AVAudioPlayer(contentsOf: url!)
+        
+        // playing the sound
+        player.play()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    //IBAction (Interface Builder Action) is a funtion which is not called by us but when an action happens on UI linked to the IBAction.. then the block of code gets executed
+    // (_ sender: UIButton) --> inputs to function
+    // Dragging the outlet connection formed in this file to all other keys on UI of Main.storyboard
+    //Now linked all the 7 buttons to below IBAction
     @IBAction func cKeyClicked(_ sender: UIButton) {
-        print("c key pressed")
+        print(sender.backgroundColor)// prints the color of sender button hich is basically the button tapped by user
+        
+        /*
+         We can get the title of button in 3 ways :
+         1. sender.title(for: .normal)
+         2. sender.titleLabel?.text
+         3. sender.currentTitle
+        */
+        print(sender.currentTitle)
         playSound()
     }
 }
