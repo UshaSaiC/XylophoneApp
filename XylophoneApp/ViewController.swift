a1a8c85e622c8d9d1ee1e1b01db7ad5146e68931
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-04 12:18:49 +0530

    Ensuring when C key is tapped on screen, then a sund gets played

--- XylophoneApp/ViewController.swift
+++ XylophoneApp/ViewController.swift
@@ -6,8 +6,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
+
+    // below function is copy pasted from stack over flow
+    func playSound() {
+        
+        // getting C.wav file from the resources in this project using Bundle.main keyword
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+
+        do {
+            // making sure that audio is played even when phone is silent via using category as .playback
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            // telling audio player that we have to play url file i.e C.wav file
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +41,9 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func cKeyClicked(_ sender: UIButton) {
+        print("c key pressed")
+        playSound()
+    }
 }
 
